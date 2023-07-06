c24e0a9fe22ece84d3d2300e3195a6f92d91471b
Thanet S <ai.thanet@gmail.com>
2023-07-06 16:47:37 +0700

    Fix Bug

--- com.my.charoenpattana.panel/test.jsx
+++ com.my.charoenpattana.panel/test.jsx
@@ -3,4 +3,10 @@
 #include "./jsx/utils.jsx"
 
 
-startChangeName(["C:\\Users\\AI_th\\OneDrive\\Desktop\\2018-V328\\2018-V328\\V328 all.indd"], "V328", "HG328")
+//startChangeName(["C:\\Users\\AI_th\\OneDrive\\Desktop\\2018-V328\\2018-V328\\V328 all.indd"], "V328", "HG328")
+
+priceList = {
+    "HG328":1100.5    
+}
+
+startUpdatePrice(["C:\\Users\\AI_th\\OneDrive\\Desktop\\2018-V328\\2018-V328\\HG328-01.indd",priceList]
