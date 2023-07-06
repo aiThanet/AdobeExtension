502249a439438a09f6a133fa1d5cd91b15d60f81
Thanet S <ai.thanet@gmail.com>
2023-07-06 14:39:14 +0700

    Add test

---
+++ com.my.charoenpattana.panel/test.jsx
@@ -0,0 +1,6 @@
+﻿#include "./jsx/index.jsx"
+#include "./jsx/json.jsx"
+#include "./jsx/utils.jsx"
+
+
+startChangeName(["C:\\Users\\AI_th\\OneDrive\\Desktop\\2018-V328\\2018-V328\\V328 all.indd"], "V328", "HG328")
