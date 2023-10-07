c2ad1754fcca626665b18e05f9cb2ddceff417cd
Thanet <ai.thanet@gmail.com>
2023-10-07 14:06:53 +0700

    Update trim.py

--- com.my.charoenpattana.panel/jsx/trim.py
+++ com.my.charoenpattana.panel/jsx/trim.py
@@ -7,10 +7,13 @@ def trim_all_images(folder_path):
 
 def trim_image(image_path):
     im = Image.open(image_path)
-    im.size 
-    im.getbbox()
+    width, height = im.size
+    bbox = im.getbbox()
+    b_width = bbox[2] - bbox[0]
+    b_height = bbox[3] - bbox[1]
+    if(b_width == width and b_height == height):
+        return
     im2 = im.crop(im.getbbox())
-    im2.size
     im2.save(image_path)
 
 trim_all_images('\\\\JPNNAS\\jpndesign\\images\\withoutDescription')
