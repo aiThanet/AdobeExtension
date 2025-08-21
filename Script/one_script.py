0de5021d12ffcbac2eab23a507ea6088917c2167
CharoenpattanaDesign <charoenpattan.design@outlook.co.th>
2025-08-21 16:36:40 +0700

    feat: add pdf2image

--- Script/one_script.py
+++ Script/one_script.py
@@ -3,6 +3,11 @@ from glob import glob
 from tqdm import tqdm
 from pathlib import Path
 
+import os
+import img2pdf
+from datetime import datetime
+from pypdf import PdfMerger, PdfWriter
+from pdf2image import convert_from_path
 
 def add_watermask_all_images(folder_path, watermask_path, output_path):
     page = 1
@@ -42,21 +47,14 @@ def add_watermask(image_path, watermask_path, output_path, page_number=None):
     background.save(output_path + '\\' + Path(image_path).stem + '.jpg', optimize=True, dpi=(300, 300))
 
 
-print("Step1/4: Adding watermarks and page number to images...")
+print("Step1/3: Adding watermarks and page number to images...")
 input_folder = 'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\00 ต้นฉบับ'
 output_folder = 'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\00 ต้นฉบับ ลายน้ำ'
 watermask_path = './Watermask_5.jpg'
 add_watermask_all_images(input_folder, watermask_path, output_folder)
 
 
-import img2pdf
-import glob
-from datetime import datetime
-from pypdf import PdfMerger, PdfWriter
-from pdf2image import convert_from_path
-
-
-print("Step2/4: Converting images to PDF...")
+print("Step2/3: Converting images to PDF...")
 output_folder = 'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\00 ต้นฉบับ ลายน้ำ\\'
 output_file = 'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\01 แยกไฟล์ PDF\\03 เนื้อหา.pdf'
 # specify paper size (A4)
@@ -64,12 +62,12 @@ a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
 layout_fun = img2pdf.get_layout_fun(a4inpt)
 	
 with open(output_file,"wb") as f:
-	f.write(img2pdf.convert(glob.glob(output_folder + "*.jpg"), layout_fun=layout_fun))
+	f.write(img2pdf.convert(glob(output_folder + "*.jpg"), layout_fun=layout_fun))
 
-print("Step3/4: Combining PDFs and save...")
+print("Step3/3: Combining PDFs and save...")
 merger = PdfWriter()
 pdf_folder = 'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\01 แยกไฟล์ PDF\\'
-pdfs = glob.glob(pdf_folder + "*.pdf")
+pdfs = glob(pdf_folder + "*.pdf")
 for pdf in pdfs:
     merger.append(pdf)
 
@@ -80,14 +78,16 @@ output_file = f'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิม
 merger.write(output_file)
 merger.close()
 	
-print("Step4/4: Exporting PDF pages to images...")
-# Folder to save images
-output_path = f'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\03 แยกหน้าเล่ม\\'
-os.makedirs(output_path, exist_ok=True)
+# print("Step4/4: Exporting PDF pages to images...")
 
-# Convert PDF to images
-pages = convert_from_path(output_file, dpi=300)
 
-for i, page in enumerate(pages, start=1):
-    image_path = os.path.join(output_path, f'page_{i}.jpg')
-    page.save(image_path, 'JPEG', quality=95)
+# # Folder to save images
+# output_path = f'C:\\Users\\jpndesign.JPN\\Documents\\ส่งโรงพิมพ์\\03 แยกหน้าเล่ม\\'
+# os.makedirs(output_path, exist_ok=True)
+# # Convert PDF to images
+# pages = convert_from_path(output_file, dpi=300, poppler_path = r"C:\Users\jpndesign.JPN\Downloads\Release-25.07.0-0\poppler-25.07.0\Library\bin" )
+
+# for i, page in tqdm(enumerate(pages, start=1)):
+#     image_path = os.path.join(output_path, f'page_{i}.jpg')
+#     page.save(image_path, 'JPEG', quality=95)
+    
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@ Blank page bug
 : enable debug mode
 
 https://github.com/Adobe-CEP/Getting-Started-guides/tree/master/Client-side%20Debugging#set-the-debug-mode
+
+
+#Installation for one_script
+1. https://pypi.org/project/pdf2image/
