398a63ec34a8dd09ce03671668a4b219befa796a
CharoenpattanaDesign <charoenpattan.design@outlook.co.th>
2025-12-01 13:47:00 +0700

    refactor: streamline file selection logic by removing unnecessary checks

--- com.my.charoenpattana.panel/client/exportPDF/index.js
+++ com.my.charoenpattana.panel/client/exportPDF/index.js
@@ -2,8 +2,12 @@ $("#folderSelector").on("change", e => {
   const dt = new DataTransfer();
 
   for (file of e.target.files) {
+
+    if(file.path.toLowerCase().indexOf("[skip]") != -1 || getFileName(file.name).toLowerCase().indexOf("[skip]") != -1) {
+      continue;
+    }
     
-    if (getExtension(file.name) == "indd" && getFileName(file.name).toLowerCase().indexOf("all") != -1 && file.path.toLowerCase().indexOf("_ignore_") == -1) {
+    if (getExtension(file.name) == "indd" && getFileName(file.name).toLowerCase().indexOf("all") != -1) {
       dt.items.add(file);
     }
   }
